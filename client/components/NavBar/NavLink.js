1370f226cf6c217497019affcb04d9f47cc4f6be
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-25 23:13:21 +0200

    add active state styles

--- client/components/NavBar/NavLink.js
+++ client/components/NavBar/NavLink.js
@@ -1,11 +1,15 @@
+import {useRouter} from 'next/router';
 import Link from "next/link";
 
 import classes from "./navLink.module.scss"
 
 const NavLink = ({name, href}) => {
+  const router = useRouter();
+  const isActive = router.pathname === href;
+  console.log(isActive);
 	return (
 		<Link href={href}>
-			<li  className={classes['body']}>
+			<li  className={`${classes.body} ${isActive?classes.active:""}`}>
 				<p>{name}</p>
 			</li>
 		</Link>
