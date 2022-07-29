77b389e6d4df660204ee91d706bea10e48bda7ad
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-07-29 18:17:54 +0200

    create new album component

---
+++ client/components/NewAlbum/NewAlbum.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import classes from "./newAlbum.module.scss";
+
+import newAlbum from "../../assets/new-album.jpeg";
+
+const NewAlbum = () => {
+	return (
+		<div className={classes.body + " section__padding"}>
+			<div className={classes["image-container"]}>
+				<Image src={newAlbum} />
+			</div>
+			<div className={classes.content}>
+        <h4 className="gradient-text">New Album</h4>
+				<p className={classes.title}>Through My Eyes</p>
+				<p className={classes.lyrics}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae magna at elit porta
+					aliquam.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default NewAlbum;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,13 @@
 import Hero from '../components/Hero/Hero';
 import NavBar from '../components/NavBar/NavBar';
+import NewAlbum from '../components/NewAlbum/NewAlbum';
 
 export default function Home() {
   return (
     <>
       <NavBar/>
       <Hero/>
+      <NewAlbum/>
     </>
   )
 }
