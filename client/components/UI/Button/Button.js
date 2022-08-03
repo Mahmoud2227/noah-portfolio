a254da88debc419bd6079f7176a34716858c9764
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-08-03 21:55:03 +0200

    fix forwardref error in Button component

--- client/components/UI/Button/Button.js
+++ client/components/UI/Button/Button.js
@@ -1,13 +1,13 @@
+import React from 'react';
+
 import classes from "./button.module.scss"
 
-const Button = ({type,onClick,children}) => {
+const Button = React.forwardRef(({type,onClick,children},ref) => {
   return (
     <button className={classes.button} type={type} onClick={onClick?onClick:null}>
       {children}
     </button>
   );
-}
-
-
+})
 
 export default Button;
