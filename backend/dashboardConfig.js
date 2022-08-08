fd9a8406773e3a65c574684b4d839f48f8bd85cb
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-08-08 15:32:47 +0200

    add dashboard

---
+++ backend/dashboardConfig.js
@@ -0,0 +1,15 @@
+export default {
+	widgets: [
+		{
+			name: "project-info",
+		},
+		{
+			name: "project-users",
+		},
+		{
+			name: "document-chart",
+			options: {types: ["concert", "author", "sanity.imageAsset"]},
+			layout: {width: "full"},
+		},
+	],
+};
