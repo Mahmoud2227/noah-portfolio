52d16c01bb4dc2e0fcaba09a8adc0356955536d9
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-08-21 23:56:46 +0200

    add google analytics

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,8 @@
 import {useState, useEffect} from "react";
 import {useRouter} from "next/router";
 import Head from "next/head";
+import Script from "next/script"
+
 import sanity from "../lib/sanity";
 
 import NavBar from "../components/NavBar/NavBar";
@@ -55,6 +57,21 @@ function MyApp({Component, pageProps}) {
 
 	return (
 		<>
+			<Script
+				strategy='lazyOnload'
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+
+			<Script strategy='lazyOnload'>
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+					page_path: window.location.pathname,
+					});
+				`}
+			</Script>
 			<Head>
 				<title>Noah Portfolio</title>
 			</Head>
