8f51d2a5850ab8306f99275e065fbaefbbf21c25
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-27 01:14:14 +0200

    animate track cover during play state

--- client/components/MusicPlayer/Track/Track.js
+++ client/components/MusicPlayer/Track/Track.js
@@ -1,24 +1,36 @@
 import Image from "next/image";
+import {motion} from "framer-motion";
+
 import imageUrlFor from "../../../utils/imageUrlFor";
 
 import classes from "./track.module.scss";
 
-const Track = ({isPlaying, isActive, activeSong, cover, albumTitle}) => (
-	<div className={classes.body}>
-		<div className={classes.coverContainer}>
-			{isActive && <Image src={imageUrlFor(cover).url()} alt='cover art' width={64} height={64} />}
+const Track = ({isPlaying, isActive, activeSong, cover, albumTitle}) => {
+	return (
+		<div className={classes.body}>
+			<motion.div
+				className={classes.coverContainer}
+				animate={
+					isPlaying
+						? {rotate: 360, transition: {duration: 5, ease: "linear", repeat: Infinity}}
+						: {rotate: 0}
+				}>
+				{isActive && (
+					<Image src={imageUrlFor(cover).url()} alt='cover art' width={64} height={64} />
+				)}
+			</motion.div>
+			<div className={classes["song-info"]}>
+				{isActive ? (
+					<>
+						<p className={classes.active}>{activeSong.title}</p>
+						<p className={classes.active}>{albumTitle}</p>
+					</>
+				) : (
+					<p className={classes.notActive}>No active Song</p>
+				)}
+			</div>
 		</div>
-		<div className={classes["song-info"]}>
-			{isActive ? (
-				<>
-					<p className={classes.active}>{activeSong.title}</p>
-					<p className={classes.active}>{albumTitle}</p>
-				</>
-			) : (
-				<p className={classes.notActive}>No active Song</p>
-			)}
-		</div>
-	</div>
-);
+	);
+};
 
 export default Track;
