f31f14012bdfdaec6fa9e37fb5feb214765576a8
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-10-14 20:36:43 +0200

    add animation

--- client/components/SettingsModal/index.js
+++ client/components/SettingsModal/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
+import {motion, AnimatePresence} from "framer-motion";
 
 import VisualizerOptions from "./VisualizerSettings/VisualizerSettings";
 import ToggleBtn from "./ToggleBtn/ToggleBtn";
@@ -7,14 +8,22 @@ import ToggleBtn from "./ToggleBtn/ToggleBtn";
 import classes from "./settingsModal.module.scss";
 
 const settingsModal = () => {
-  const isHidden = useSelector((state) => state.settings.isHidden);
+	const isHidden = useSelector((state) => state.settings.isHidden);
 	return (
 		<>
 			<ToggleBtn />
-			{!isHidden && <div className={classes.body}>
-				<h1>Settings</h1>
-				<VisualizerOptions />
-			</div>}
+			<AnimatePresence>
+				{!isHidden && (
+					<motion.div
+						className={classes.body}
+						initial={{scale: 0, x: "-50%", y: "-50%"}}
+						animate={{scale: 1, x: "-50%", y: "-50%"}}
+						exit={{scale: 0, x: "-50%", y: "-50%"}}>
+						<h1>Settings</h1>
+						<VisualizerOptions />
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</>
 	);
 };
